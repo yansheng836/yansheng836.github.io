d22a25de9624e43d6258bc0b96fcd7d5431474e0
yansheng836 <1812871846@qq.com>
2019-08-17 20:07:54 +0800

    :bug:修复相册加载不了相片的问题
    调试时，发现ins.js出错，猜测压缩该文件发生错误。
    故忽略压缩/source/photos/ins.js文件，就可以了。
     Changes to be committed:
    	modified:   _config.yml
    	modified:   source/photos/ins.js

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -1,65 +1,39 @@
 /******/
 (function(modules) { // webpackBootstrap
-  /******/ // The module cache
-  /******/
+  // The module cache
   var installedModules = {};
-  /******/
-  /******/ // The require function
-  /******/
+  // The require function
   function __webpack_require__(moduleId) {
-    /******/
-    /******/ // Check if module is in cache
-    /******/
+    // Check if module is in cache
     if (installedModules[moduleId])
-    /******/
       return installedModules[moduleId].exports;
-    /******/
-    /******/ // Create a new module (and put it into the cache)
-    /******/
+
+    // Create a new module (and put it into the cache)
     var module = installedModules[moduleId] = {
-      /******/
       exports: {},
-      /******/
       id: moduleId,
-      /******/
       loaded: false
-        /******/
     };
-    /******/
-    /******/ // Execute the module function
-    /******/
+   
+   // Execute the module function
     modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-    /******/
-    /******/ // Flag the module as loaded
-    /******/
+
+	// Flag the module as loaded
     module.loaded = true;
-    /******/
-    /******/ // Return the exports of the module
-    /******/
+	// Return the exports of the module
     return module.exports;
-    /******/
   }
-  /******/
-  /******/
-  /******/ // expose the modules object (__webpack_modules__)
-  /******/
+  // expose the modules object (__webpack_modules__)
   __webpack_require__.m = modules;
-  /******/
-  /******/ // expose the module cache
-  /******/
+  // expose the module cache
   __webpack_require__.c = installedModules;
-  /******/
-  /******/ // __webpack_public_path__
-  /******/
+  // __webpack_public_path__
   __webpack_require__.p = "/dist/";
-  /******/
-  /******/ // Load entry module and return exports
-  /******/
+  // Load entry module and return exports
   return __webpack_require__(0);
-  /******/
 })
 /************************************************************************/
-/******/
+
 ([
   /* 0 */
   /***/
@@ -207,10 +181,8 @@
 
     // export default impush;
 
-    /***/
   },
   /* 1 */
-  /***/
   function(module, exports, __webpack_require__) {
 
     /* WEBPACK VAR INJECTION */
