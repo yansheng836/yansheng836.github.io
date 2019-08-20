4cc8475d4bbbf02b5e0a7395f9f02b64ff66f5db
yansheng836 <1812871846@qq.com>
2019-08-20 10:51:20 +0800

    Site updated: 2019-08-20 10:51:14

--- photos/ins.js
+++ photos/ins.js
@@ -101,7 +101,7 @@
           src += '';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">\
-                <a href="' + src + '" itemprop="contentUrl" data-size="1920x1080" data-type="' + type + '" data-target="' + src + '">\
+                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
                   <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
