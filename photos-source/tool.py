d3459724b74f9f0e8f323061e39c11ba8e9802ba
yansheng836 <1812871846@qq.com>
2019-08-20 10:48:01 +0800

    Add photos to album

--- photos-source/tool.py
+++ photos-source/tool.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from ImageProcess import Graphics
 
 # 定义压缩比，数值越大，压缩越小
-SIZE_normal = 1.0
+SIZE_normal = 1.0	#1.0相当于不压缩
 SIZE_small = 1.5
 SIZE_more_small = 2.0
 SIZE_more_small_small = 3.0
@@ -171,9 +171,6 @@ def git_operation():
 
 if __name__ == "__main__":
     cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
-    compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
+    compress_photo()    # 压缩图片，并保存到mini_photos文件夹下
     #git_operation()    # 提交到github仓库
     handle_photo()     # 将文件处理成json格式，存到博客仓库中
-    
-    
-    
