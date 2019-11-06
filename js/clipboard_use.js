bbc35b6bf76b4cb269afd15913ebd74d13fb0330
yansheng836 <sheng.yan836@gmail.com>
2019-11-06 20:40:41 +0800

    Site updated: 2019-11-06 20:40:36

---
+++ js/clipboard_use.js
@@ -0,0 +1,3 @@
+// build time:Wed Nov 06 2019 20:28:27 GMT+0800 (GMT+08:00)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+//rebuild by neat 
